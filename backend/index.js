40eb0bc82d59aa6ef2cc5aed38d4edda53378924
vivek-stacks <vivekbhati036@hotmail.com>
2024-10-15 01:05:35 +0530

    added dependency

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/connection');
 const authRoutes = require('./routes/authetication');
 const bookRoutes = require('./routes/books');
