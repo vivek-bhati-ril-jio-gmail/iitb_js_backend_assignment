3977b23a7b096b9e7a5a99b1c10d122f91c5642e
vivek-stacks <vivekbhati036@hotmail.com>
2024-10-15 10:30:27 +0530

    chanes

--- frontend/librarian/js/manage-books.js
+++ frontend/librarian/js/manage-books.js
@@ -1,4 +1,4 @@
-const apiUrl = 'https://iitb-project-09adefc1d972.herokuapp.com/api/books'; // Update with your API endpoint
+const apiUrl = 'http://localhost:5000/api/books'; // Update with your API endpoint
 const bookList = document.getElementById('book-list');
 const bookModal = document.getElementById('book-modal');
 const bookHistoryList = document.getElementById('book-history-list');
@@ -61,7 +61,7 @@ async function displayBooks(books, totalPages) {
             bookList.appendChild(noResultsItem);
         } else {
             books.forEach(book => {
-                const remainingCount = book.numberOfCopies - book.borrowedBy.length;
+                const remainingCount = getAvailableCount(book.borrowedBy, book.numberOfCopies);
                 const bookItem = document.createElement('li');
                 bookItem.className = 'book-item';
                 bookItem.innerHTML = `
@@ -166,7 +166,7 @@ updateBookBtn.addEventListener('click', async () => {
         author: authorInput.value,
         numberOfCopies: numberOfCopiesInput.value
     };
-    await fetch(`https://iitb-project-09adefc1d972.herokuapp.com/api/books/${currentBookId}`, { // Corrected line
+    await fetch(`http://localhost:5000/api/books/${currentBookId}`, { // Corrected line
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -181,7 +181,7 @@ updateBookBtn.addEventListener('click', async () => {
 // Delete User
 async function deleteBook(id) {
     if (confirm('Are you sure you want to delete this book?')) {
-        const response = await fetch(`https://iitb-project-09adefc1d972.herokuapp.com/api/books/${id}`, {
+        const response = await fetch(`http://localhost:5000/api/books/${id}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
@@ -227,6 +227,19 @@ document.getElementById('items-per-page').addEventListener('change', (event) => 
     currentPage = 1; // Reset to the first page
     loadBooks(currentPage);
 });
+
+function getAvailableCount(borrowedBy, numberOfCopies) {
+    // Count how many times 'BORROWED' appears in the array
+    const borrowedCount = borrowedBy.filter(borrow => borrow.action === 'BORROWED').length;
+    
+    // Count how many times 'RETURNED' appears in the array
+    const returnedCount = borrowedBy.filter(borrow => borrow.action === 'RETURNED').length;
+
+    // Calculate available count
+    const availableCount = numberOfCopies - (borrowedCount - returnedCount);
+    
+    return availableCount;
+}
 
 // Initial load
 loadBooks(currentPage);
